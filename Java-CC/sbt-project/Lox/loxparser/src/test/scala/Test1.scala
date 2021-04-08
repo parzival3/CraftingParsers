e206f73e54ac9ba30438a117d8ffe0006b4e50ac
Enrico Tolotto <etolotto@gmail.com>
2021-04-08 18:15:07 +0200

    javacc: Lox: fixing unary expressions

--- Java-CC/sbt-project/Lox/loxparser/src/test/scala/Test1.scala
+++ Java-CC/sbt-project/Lox/loxparser/src/test/scala/Test1.scala
@@ -3,7 +3,7 @@ import org.junit.Assert._
 
 class Test1 {
   @Test def t1(): Unit = {
-    val test_expression =  "8 * 3 * (2 + 2345234.22) * (\"true\")"
+    val test_expression =  "8 * 3 * (2 + 2345234.22) * (\"true\") * (----!!!!3)"
     val tree: Expr =  new SyntaxChecker(new java.io.StringReader(test_expression)).expression()
     println(prettyPrint(tree))
   }
